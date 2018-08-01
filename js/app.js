85fe1c77d4ded137d6b40c550ca324cc87590f31
Cristina <elena.cristina.dinca@gmail.com>
2018-08-01 07:21:54 +0300

    Twoo cats

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
-let count = 0;
-$('#click').click(function(e) {
-  count = count + 1;
-  document.getElementById("counter").innerHTML = "You clicked " + count + " times ";
+
+let name1 = "Tomas";
+let name2 = "SweetPie";
+document.getElementById("catName1").innerHTML = "The cat's name is: " + name1;
+document.getElementById("catName2").innerHTML = "The cat's name is: " + name2;
+let count1 = 0;
+$('#click1').click(function(e) {
+  count1 = count1 + 1;
+  document.getElementById("counter1").innerHTML = "You clicked " + name1 + " " + count1 + " times ";
+});
+
+let count2 = 0;
+$('#click2').click(function(e) {
+  count2 = count2 + 1;
+  document.getElementById("counter2").innerHTML = "You clicked " + name2 + " " + count2 + " times ";
 });
