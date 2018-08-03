9ec081688b7faf52e3a46776cfcf17cb935f9dc3
CristinaElenaDinca <37436448+CristinaElenaDinca@users.noreply.github.com>
2018-08-03 15:55:06 +0300

    Try to modify the click event on the immage`

--- js/app.js
+++ js/app.js
@@ -1,16 +1,64 @@
 
-let name1 = "Tomas";
-let name2 = "SweetPie";
-document.getElementById("catName1").innerHTML = "The cat's name is: " + name1;
-document.getElementById("catName2").innerHTML = "The cat's name is: " + name2;
-let count1 = 0;
-$('#click1').click(function(e) {
-  count1 = count1 + 1;
-  document.getElementById("counter1").innerHTML = "You clicked " + name1 + " " + count1 + " times ";
-});
+class Cat {
+	constructor  (name, source) {
+		this.name = name;
+		this.image = source;
+		this.count = 0;
+	}
+}
 
-let count2 = 0;
-$('#click2').click(function(e) {
-  count2 = count2 + 1;
-  document.getElementById("counter2").innerHTML = "You clicked " + name2 + " " + count2 + " times ";
-});
+let arrayOfCats = [];
+
+arrayOfCats.push(
+	new Cat("Thomas", "img/babyCat.jpg"), 
+	new Cat("Sweetie", "img/CatEyes.jpg"), 
+	new Cat("John", "img/catmoney.jpg"),
+	new Cat("Happy", "img/GrumpyCat.jpg"),
+	new Cat("Prince", "img/YellowEyes.jpg"),
+	new Cat("Sir", "img/mustace.jpg"),
+);
+
+
+//Creating the list of cats to be displayed
+for (let i = 0; i <(arrayOfCats.length+1); i++) {
+	//Creating a DOM element
+    var elem = document.createElement('li');
+    elem.textContent = arrayOfCats[i].name;
+    //Adding the DOM element to the document
+	let listOfCat = document.getElementById("list_of_cats");
+	listOfCat.appendChild(elem);
+	//adding an event listener
+	elem.addEventListener('click', (function(catCopy) {
+        return function() {
+            //cleaning the display section
+            document.getElementById('displaySection').innerHTML = '';
+            //display on the section the name of the cat and the image
+            document.getElementById('displaySection').innerHTML = 'You have clicked on ' 
+            + catCopy.name + ' ' + catCopy.count + 
+            ' times! This is ' + catCopy.name + ' <img src =' + catCopy.image + '>'
+        };
+    })(arrayOfCats[i]));
+}
+
+//Set the event for the immage
+
+for (let i = 0; i < arrayOfCats.length+1; i++) {
+	let cat = arrayOfCats[i];
+	let imageOfCat = arrayOfCats[i].image;
+	//adding an event listener
+	elem.addEventListener('click', (function(catCopy) {
+        return function() {
+            //cleaning the display section
+            document.getElementById('displaySection').innerHTML = '';
+            //increment the nomber of clicks
+            catCopy.count++;
+            //renders the updated info
+            document.getElementById('displaySection').innerHTML = 'You have clicked on ' 
+            + catCopy.name + ' ' + catCopy.count + 
+            ' times! This is ' + catCopy.name + ' <img src =' + catCopy.image + '>'
+        };
+	})(imageOfCat));
+};
+
+
+   
